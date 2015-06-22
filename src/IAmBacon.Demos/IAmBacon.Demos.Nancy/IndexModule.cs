a08003ad354dc58c1f29fb71722aa54108e34512
iambacon <iambacon@users.noreply.github.com>
2015-06-22 13:42:07 +0100

    :tada: Added ASP.NET Nancy project

---
+++ src/IAmBacon.Demos/IAmBacon.Demos.Nancy/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace IAmBacon.Demos.Nancy
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
