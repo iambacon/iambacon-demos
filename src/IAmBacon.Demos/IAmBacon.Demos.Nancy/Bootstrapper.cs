b02cdf0c7e676dacfd3848169d3ac8a618f2aecd
Colin Bacon <github@iambacon.co.uk>
2016-03-24 21:30:48 +0000

    Enable global error details

--- src/IAmBacon.Demos/IAmBacon.Demos.Nancy/Bootstrapper.cs
+++ src/IAmBacon.Demos/IAmBacon.Demos.Nancy/Bootstrapper.cs
@@ -2,10 +2,20 @@
 
 namespace IAmBacon.Demos.Nancy
 {
+    using global::Nancy.Bootstrapper;
+    using global::Nancy.TinyIoc;
+
     public class Bootstrapper : DefaultNancyBootstrapper
     {
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            StaticConfiguration.DisableErrorTraces = false;
+        }
     }
 }
